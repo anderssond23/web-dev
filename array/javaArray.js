5150ceffe3f3872994c65ef7748be1b699afedc0
David <anderssond@mssm.org>
2022-09-07 08:57:05 -0400

    homework

---
+++ array/javaArray.js
@@ -0,0 +1,7 @@
+let words = ["funny", "money", "honey", "bunny"];
+
+for (let i of words){
+let el = document.createElement('p');
+document.querySelector('body').appendChild(el);
+package.appendChild(i);
+}
